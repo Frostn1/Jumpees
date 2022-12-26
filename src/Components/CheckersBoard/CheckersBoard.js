b96b0e761bc4fade20c971b6684515f291ec1772
sean <sean@lynxight.com>
2022-12-27 01:03:35 +0200

    Fixed self eat bug

--- src/Components/CheckersBoard/CheckersBoard.js
+++ src/Components/CheckersBoard/CheckersBoard.js
@@ -84,7 +84,7 @@ const jumpees_board = [
     {
       clicked: false, path: false, piece: null
     }, {
-    clicked: false, path: false, piece: null
+    clicked: false, path: false, piece: 1
   }, {
     clicked: false, path: false, piece: null
   }, {
@@ -113,7 +113,7 @@ const jumpees_board = [
   }, {
     clicked: false, path: false, piece: null
   }, {
-    clicked: false, path: false, piece: 1
+    clicked: false, path: false, piece: 3
   }, {
     clicked: false, path: false, piece: null
   }
@@ -170,6 +170,8 @@ const CheckersBoard = (props) => {
   const [colorPallete] = useState(props.colorPallete)
   const [gameOver, setGameOver] = useState(false);
   const [playerWon, setPlayerWon] = useState(null);
+  const [lightCount, setLightCount] = useState(12);
+  const [darkCount, setDarkCount] = useState(12);
   const clearSelection = () => {
     selectedValidMoves.forEach((value, _oldValidMove) => {
       const parsedValidMove = JSON.parse(_oldValidMove)
@@ -184,35 +186,35 @@ const CheckersBoard = (props) => {
   const getJumpPaths = (rowIndex, columnIndex, pieceColor, oldValidMoves) => {
     // [[x,y]...] which are valid moves
     const validMoves = new Map();
-    if( (oldValidMoves.size || (pieceColor === 0)) && rowIndex + 2 < BOARD_SIZE && columnIndex + 2 < BOARD_SIZE && liveBoard[rowIndex + 1][columnIndex + 1].piece !== null && liveBoard[rowIndex + 2][columnIndex + 2].piece === null && !oldValidMoves.has(JSON.stringify([rowIndex + 2, columnIndex + 2]))) {
+    if( (oldValidMoves.size || (pieceColor === 0)) && rowIndex + 2 < BOARD_SIZE && columnIndex + 2 < BOARD_SIZE && liveBoard[rowIndex + 1][columnIndex + 1].piece !== null && (liveBoard[rowIndex + 1][columnIndex + 1].piece !== pieceColor && Math.abs(liveBoard[rowIndex + 1][columnIndex + 1].piece - pieceColor) !== 2)  && liveBoard[rowIndex + 2][columnIndex + 2].piece === null && !oldValidMoves.has(JSON.stringify([rowIndex + 2, columnIndex + 2]))) {
       validMoves.set(JSON.stringify([rowIndex + 2, columnIndex + 2]), [[rowIndex, columnIndex]])
       const tempValidMoves = getJumpPaths(rowIndex + 2, columnIndex + 2, pieceColor, new Map([...validMoves, ...oldValidMoves]));
       tempValidMoves.forEach((value, _oldValidMove) => {
-        const pushedValue = [...value, [rowIndex, columnIndex]]
+        const pushedValue = [[rowIndex, columnIndex], ...value]
         validMoves.set(_oldValidMove, pushedValue)
       })
     }
-    if((oldValidMoves.size || (pieceColor === 0)) && rowIndex + 2 < BOARD_SIZE && columnIndex - 2 >= 0 && liveBoard[rowIndex + 1][columnIndex - 1].piece !== null && liveBoard[rowIndex + 2][columnIndex - 2].piece === null && !oldValidMoves.has(JSON.stringify([rowIndex + 2, columnIndex - 2]))) {
+    if((oldValidMoves.size || (pieceColor === 0)) && rowIndex + 2 < BOARD_SIZE && columnIndex - 2 >= 0 && liveBoard[rowIndex + 1][columnIndex - 1].piece !== null && (liveBoard[rowIndex + 1][columnIndex + 1].piece !== pieceColor && Math.abs(liveBoard[rowIndex + 1][columnIndex + 1].piece - pieceColor) !== 2)   && liveBoard[rowIndex + 2][columnIndex - 2].piece === null && !oldValidMoves.has(JSON.stringify([rowIndex + 2, columnIndex - 2]))) {
       validMoves.set(JSON.stringify([rowIndex + 2, columnIndex - 2]), [[rowIndex, columnIndex]])
       const tempValidMoves = getJumpPaths(rowIndex + 2, columnIndex - 2, pieceColor, new Map([...validMoves, ...oldValidMoves]));
       tempValidMoves.forEach((value, _oldValidMove) => {
-        const pushedValue = [...value, [rowIndex, columnIndex]]
+        const pushedValue = [[rowIndex, columnIndex], ...value]
         validMoves.set(_oldValidMove, pushedValue)
       })
     }
-    if((oldValidMoves.size || (pieceColor === 1)) && rowIndex - 2 >= 0 && columnIndex + 2 < BOARD_SIZE && liveBoard[rowIndex - 1][columnIndex + 1].piece !== null && liveBoard[rowIndex - 2][columnIndex + 2].piece === null && !oldValidMoves.has(JSON.stringify([rowIndex - 2, columnIndex + 2]))) {
+    if((oldValidMoves.size || (pieceColor === 1)) && rowIndex - 2 >= 0 && columnIndex + 2 < BOARD_SIZE && liveBoard[rowIndex - 1][columnIndex + 1].piece !== null && (liveBoard[rowIndex + 1][columnIndex + 1].piece !== pieceColor && Math.abs(liveBoard[rowIndex + 1][columnIndex + 1].piece - pieceColor) !== 2)   && liveBoard[rowIndex - 2][columnIndex + 2].piece === null && !oldValidMoves.has(JSON.stringify([rowIndex - 2, columnIndex + 2]))) {
       validMoves.set(JSON.stringify([rowIndex - 2, columnIndex + 2]), [[rowIndex, columnIndex]])
       const tempValidMoves = getJumpPaths(rowIndex - 2, columnIndex + 2, pieceColor, new Map([...validMoves, ...oldValidMoves]));
       tempValidMoves.forEach((value, _oldValidMove) => {
-        const pushedValue = [...value, [rowIndex, columnIndex]]
+        const pushedValue = [[rowIndex, columnIndex], ...value]
         validMoves.set(_oldValidMove, pushedValue)
       })
     }
-    if((oldValidMoves.size || pieceColor === 1) && rowIndex - 2 >= 0 && columnIndex - 2 >= 0 && liveBoard[rowIndex - 1][columnIndex - 1].piece !== null && liveBoard[rowIndex - 2][columnIndex - 2].piece === null && !oldValidMoves.has(JSON.stringify([rowIndex - 2, columnIndex - 2]))) {
+    if((oldValidMoves.size || pieceColor === 1) && rowIndex - 2 >= 0 && columnIndex - 2 >= 0 && liveBoard[rowIndex - 1][columnIndex - 1].piece !== null && (liveBoard[rowIndex + 1][columnIndex + 1].piece !== pieceColor && Math.abs(liveBoard[rowIndex + 1][columnIndex + 1].piece - pieceColor) !== 2)   && liveBoard[rowIndex - 2][columnIndex - 2].piece === null && !oldValidMoves.has(JSON.stringify([rowIndex - 2, columnIndex - 2]))) {
       validMoves.set(JSON.stringify([rowIndex - 2, columnIndex - 2]), [[rowIndex, columnIndex]])
       const tempValidMoves = getJumpPaths(rowIndex - 2, columnIndex - 2, pieceColor, new Map([...validMoves, ...oldValidMoves]));
       tempValidMoves.forEach((value, _oldValidMove) => {
-        const pushedValue = [...value, [rowIndex, columnIndex]]
+        const pushedValue = [[rowIndex, columnIndex], ...value]
         validMoves.set(_oldValidMove, pushedValue)
       })
     }
@@ -265,35 +267,48 @@ const CheckersBoard = (props) => {
     jumpees_board[rowIndex][columnIndex].clicked = true;
     setLiveBoard(jumpees_board)
   }
+  const checkWin = () => {
+    if(!lightCount) {
+      setGameOver(true);
+      setPlayerWon(0)
+    }
 
+    if(!darkCount) {
+      setGameOver(true);
+      setPlayerWon(1)
+    }
+  }
   const movePiece = (destRowIndex, destColumnIndex, srcRowIndex, srcColumnIndex, piece) => {
     if(liveBoard[destRowIndex][destColumnIndex].path) {
       const destTuple = JSON.stringify([destRowIndex, destColumnIndex]);
       const selectedMove = selectedValidMoves.get(destTuple);
       if(Array.isArray(selectedMove))
         selectedMove.forEach((move, index) => {
-          let xCoord = 0
-          let yCoord = 0
-          if(index === 0) {
-            xCoord = (move[0] + destRowIndex) / 2
-            yCoord = (move[1] + destColumnIndex) / 2
-          } else if (index < selectedMove.length - 1) {
+          let xCoord = 0;
+          let yCoord = 0;
+          console.log('sean .. move', move[0], move[1]);
+          if (index < selectedMove.length - 1) {
             xCoord = (move[0] + selectedMove[index + 1][0]) / 2
             yCoord = (move[1] + selectedMove[index + 1][1]) / 2
           } else {
-            xCoord = (move[0] + selectedMove[index - 1][0]) / 2
-            yCoord = (move[1] + selectedMove[index - 1][1]) / 2
+            xCoord = (move[0] + destRowIndex) / 2
+            yCoord = (move[1] + destColumnIndex) / 2
           }
+
+
+
+          jumpees_board[xCoord][yCoord].piece === 1 ? setLightCount(lightCount - 1) : setDarkCount(darkCount - 1);
           jumpees_board[xCoord][yCoord].piece = null;
-          console.log('sean .. xc yc', xCoord, yCoord)
         })
-      console.log('sean .. clicked move', destRowIndex, destColumnIndex, selectedMove)
       jumpees_board[srcRowIndex][srcColumnIndex].piece = null;
       jumpees_board[destRowIndex][destColumnIndex].piece = piece;
     }
     setLiveBoard(jumpees_board)
     setCurrentPlayer(currentPlayer ? 0 : 1)
     setMoveCounter(moveCounter + 1)
+    if(moveCounter > 15) {
+      checkWin();
+    }
     clearSelection();
   }
 
@@ -348,7 +363,7 @@ const CheckersBoard = (props) => {
                                 ${square.path && props.showPaths ? 'path-color' : ''}`} onClick={square.path ? () => {movePiece(rowIndex, columnIndex, selectedRowIndex, selectedColumnIndex, liveBoard[selectedRowIndex][selectedColumnIndex].piece)} : () => {}}>
                             {square.piece !== null ?
                                 <div
-                                    className={`piece ${square.piece === 1 ? 'light-piece-color' : 'dark-piece-color'} 
+                                    className={`${square.piece === 1 || square.piece === 0 ? 'piece' : 'queen'} ${square.piece === 1 || square.piece === 3 ? 'light-piece-color' : 'dark-piece-color'} 
                                `} onClick={() => {onClick(rowIndex, columnIndex)}}/>
                                 : null}
                           </div>
